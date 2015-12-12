5992c93b98d66f5a8552899f3541773dfc70d326
svetlana_739 <svt.and.739@gmail.com>
2015-12-12 19:38:31 +0600

    work wich Django

---
+++ polls/migrations/0002_question_my_field.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='question',
+            name='my_field',
+            field=models.CharField(default='SOME STRING', max_length=200),
+        ),
+    ]
