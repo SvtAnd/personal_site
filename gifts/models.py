125860ea135e11c3169c0f0f0aa901a1c432e9f3
svetlana_739 <svt.and.739@gmail.com>
2016-01-02 21:02:25 +0600

    new models

---
+++ gifts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from Cython.Compiler.TypeSlots import descrdelfunc
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    img = models.ImageField(upload_to="article_images")
+    thumb = models.ImageField(upload_to="article_thumbs")
