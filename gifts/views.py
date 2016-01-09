49c03d134835f84d7cf5362ac5ad667b9ca8ef2f
svetlana_739 <svt.and.739@gmail.com>
2016-01-10 00:24:44 +0600

    ready site

--- gifts/views.py
+++ gifts/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
+# coding: utf-8
+
 from django.views.generic.base import TemplateView
+from .models import Article
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = "index.html"
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
